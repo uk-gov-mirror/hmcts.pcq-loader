aec464fee4bc8e763d2ff4da15202f2d95d232f6
Dave Jones <dave.jones@hmcts.net>
2020-08-04 17:28:42 +0100

    PCQ-588: Correction to variables.

--- infrastructure/storage-account.tf
+++ infrastructure/storage-account.tf
@@ -5,13 +5,14 @@ provider "azurerm" {
 locals {
   vault_name                = "${var.product}-${var.env}"
   resource_group_name       = "${var.product}-${var.env}"
+  storage_account_name      = "${var.product}shared${var.env}"
 }
 
 // pcq blob Storage Account
 module "pcq_storage_account" {
   source                    = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
   env                       = "${var.env}"
-  storage_account_name      = "${var.product}shared${var.env}"
+  storage_account_name      = "${local.storage_account_name}"
   resource_group_name       = "${local.resource_group_name}"
   location                  = "${var.location}"
   account_kind              = "StorageV2"
@@ -24,9 +25,9 @@ module "pcq_storage_account" {
   enable_https_traffic_only = true
 
   // Tags
-  common_tags            = "${var.common_tags}"
-  team_contact = "${var.team_contact}"
-  destroy_me   = "${var.destroy_me}"
+  common_tags               = "${var.common_tags}"
+  team_contact              = "${var.team_contact}"
+  destroy_me                = "${var.destroy_me}"
 }
 
 data "azurerm_key_vault" "key_vault" {
@@ -36,46 +37,38 @@ data "azurerm_key_vault" "key_vault" {
 
 resource "azurerm_storage_container" "pcq_containers" {
   name                  = "pcq"
-  storage_account_name  = "${module.pcq_storage_account.storage_account_name}"
+  storage_account_name  = "${local.storage_account_name}"
   container_access_type = "private"
 }
 
 // pcq blob Storage Account Vault Secrets
 resource "azurerm_key_vault_secret" "pcq_storageaccount_id" {
-  depends_on = ["module.vault"]
   name      = "pcq-storage-account-id"
   value     = "${module.pcq_storage_account.storageaccount_id}"
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
 }
 
 resource "azurerm_key_vault_secret" "pcq_storageaccount_primary_access_key" {
-  depends_on = ["module.vault"]
   name      = "pcq-storage-account-primary-access-key"
   value     = "${module.pcq_storage_account.storageaccount_primary_access_key}"
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
 }
 
 resource "azurerm_key_vault_secret" "pcq_storageaccount_secondary_access_key" {
-  depends_on = ["module.vault"]
   name      = "pcq-storage-account-secondary-access-key"
   value     = "${module.pcq_storage_account.storageaccount_secondary_access_key}"
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
 }
 
 resource "azurerm_key_vault_secret" "pcq_storageaccount_primary_connection_string" {
-  depends_on = ["module.vault"]
   name      = "pcq-storage-account-primary-connection-string"
   value     = "${module.pcq_storage_account.storageaccount_primary_connection_string}"
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
 }
 
 resource "azurerm_key_vault_secret" "pcq_storageaccount_secondary_connection_string" {
-  depends_on = ["module.vault"]
   name      = "pcq-storage-account-secondary-connection-string"
   value     = "${module.pcq_storage_account.storageaccount_secondary_connection_string}"
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
 }
 
-output "pcq_storage_account_name" {
-  value = "${module.pcq_storage_account.storageaccount_name}"
-}
