ee0c30935bdb708946df463cd5ce4fe4e8cc0836
Manoj Kulkarni <manoj.kulkarni@cgi.com>
2020-08-20 17:35:15 +0100

    Initial code

---
+++ src/main/java/uk/gov/hmcts/reform/pcqloader/helper/PayloadMappingHelper.java
@@ -0,0 +1,12 @@
+package uk.gov.hmcts.reform.pcqloader.helper;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.pcqloader.model.PcqAnswerRequest;
+
+@Component
+public class PayloadMappingHelper {
+
+    public PcqAnswerRequest mapPayLoadToPcqAnswers(String jsonPayload) {
+        return null;
+    }
+}
