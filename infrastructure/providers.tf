ca83d5d2f7113547328d57448f7e719f6597000f
Dave Jones <dave.jones@hmcts.net>
2020-09-18 00:13:55 +0100

    PCQ-653: Updated rules to add preview.

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -8,6 +8,11 @@ provider "azurerm" {
 }
 
 provider "azurerm" {
+  alias           = "aks-preview"
+  subscription_id = "${var.aks_preview_subscription_id}"
+}
+
+provider "azurerm" {
   alias           = "mgmt"
   subscription_id = "${var.mgmt_subscription_id}"
 }
