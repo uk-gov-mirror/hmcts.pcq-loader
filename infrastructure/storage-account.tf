8a2141492388b18f704c94f6986b591c66c36783
Dave Jones <dave.jones@hmcts.net>
2020-09-18 09:49:51 +0100

    PCQ-653: Updated rules to add preview.

--- infrastructure/storage-account.tf
+++ infrastructure/storage-account.tf
@@ -88,7 +88,7 @@ data "azurerm_subnet" "aks-01-infra" {
 data "azurerm_virtual_network" "aks_preview_vnet" {
   provider            = "azurerm.aks-preview"
   name                = "core-${var.env}-vnet"
-  resource_group_name = "aks-infra-${var.env}-rg"
+  resource_group_name = "aks-infra-preview-rg"
 }
 
 data "azurerm_subnet" "aks-00-preview" {
