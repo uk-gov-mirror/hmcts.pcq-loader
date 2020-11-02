76c1a0e4b60907e909cdfc87033b20c5344d6b09
Manoj Kulkarni <manoj.kulkarni@cgi.com>
2020-11-02 11:31:56 +0000

    Moved summary logging to utility class

--- src/main/java/uk/gov/hmcts/reform/pcqloader/PcqLoaderComponent.java
+++ src/main/java/uk/gov/hmcts/reform/pcqloader/PcqLoaderComponent.java
@@ -14,36 +14,25 @@ import uk.gov.hmcts.reform.pcq.commons.utils.PcqUtils;
 import uk.gov.hmcts.reform.pcqloader.helper.PayloadMappingHelper;
 import uk.gov.hmcts.reform.pcqloader.services.BlobStorageManager;
 import uk.gov.hmcts.reform.pcqloader.services.PcqBackendService;
+import uk.gov.hmcts.reform.pcqloader.utils.LogSummaryUtils;
 import uk.gov.hmcts.reform.pcqloader.utils.ZipFileUtils;
 
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 @Slf4j
 public class PcqLoaderComponent {
 
     private static final int MAX_RETRIES = 3;
-    private static final String SUMMARY_HEADING_STRING = "\r\nPCQ Loader Record Summary : ";
-    private static final String FORMAT_STR_LENGTH_30 = "%1$-30s";
-    private static final String SERVICE_SUMMARY_STRING = String.format(FORMAT_STR_LENGTH_30, "Service")
-        + "Created | Errors\r\n";
-    private static final String CR_STRING = "\r\n";
-    private static final String TAB_STRING = "| ";
     private static final String ERROR_SUFFIX = "_Erred";
     private static final String CRATED_SUFFIX = "_Created";
-    private static final String FORMAT_STR_LENGTH_8 = "%1$-8s";
-    private static final String TOTAL_STRING = "Total";
+
 
     private final Map<String, Integer> serviceSummaryMap = new ConcurrentHashMap<>();
 
@@ -121,7 +110,7 @@ public class PcqLoaderComponent {
                 fileUtil.deleteFilesFromLocalStorage(blobZipDirectory, unzippedFiles);
             }
         }
-        logSummary();
+        LogSummaryUtils.logSummary(serviceSummaryMap);
 
         log.info("PcqLoaderComponent finished.");
     }
@@ -181,60 +170,5 @@ public class PcqLoaderComponent {
             int count = serviceSummaryMap.get(service) + 1;
             serviceSummaryMap.put(service, count);
         }
-    }
-
-    private void logSummary() {
-        StringBuilder stringBuilder = new StringBuilder(getSummaryString());
-
-        AtomicInteger totalCreated = new AtomicInteger();
-        AtomicInteger totalErrors = new AtomicInteger();
-
-        stringBuilder.append(getServiceSummaryString(totalCreated, totalErrors))
-            .append(String.format(FORMAT_STR_LENGTH_30,TOTAL_STRING))
-            .append(String.format(FORMAT_STR_LENGTH_8,totalCreated.intValue()))
-            .append(TAB_STRING)
-            .append(totalErrors.intValue());
-
-        log.info(stringBuilder.toString());
-    }
-
-    private String getSummaryString() {
-        StringBuilder stringBuilder = new StringBuilder(SUMMARY_HEADING_STRING);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, dd MMMMM yyyy", Locale.UK);
-        stringBuilder.append(dateFormat.format(new Date()))
-            .append(CR_STRING)
-            .append(SERVICE_SUMMARY_STRING)
-            .append("-----------------------------------------------------------")
-            .append(CR_STRING);
-        return stringBuilder.toString();
-    }
-
-    private String getServiceSummaryString(AtomicInteger totalCreated, AtomicInteger totalErrors) {
-        Set<String> serviceKeySet = serviceSummaryMap.keySet();
-        StringBuilder stringBuilder = new StringBuilder();
-
-        serviceKeySet.forEach(service -> {
-            String jurisdiction = service.substring(0, service.indexOf("_"));
-            if (jurisdiction.isBlank()) {
-                stringBuilder.append(String.format(FORMAT_STR_LENGTH_30,"UNKNOWN"));
-            } else {
-                stringBuilder.append(String.format(FORMAT_STR_LENGTH_30, jurisdiction.toUpperCase(Locale.UK)));
-            }
-            Integer createdCount = serviceSummaryMap.get(jurisdiction + CRATED_SUFFIX);
-            Integer erredCount =  serviceSummaryMap.get(jurisdiction + ERROR_SUFFIX);
-            stringBuilder.append(countsString(createdCount, erredCount));
-            totalCreated.addAndGet(createdCount == null ? 0 : createdCount);
-            totalErrors.addAndGet(erredCount == null ? 0 : erredCount);
-        });
-
-        return stringBuilder.toString();
-    }
-
-    private String countsString(Integer createdCount, Integer erredCount) {
-
-        return String.format(FORMAT_STR_LENGTH_8, createdCount == null ? 0 : createdCount)
-            + TAB_STRING
-            + (erredCount == null ? 0 : erredCount)
-            + CR_STRING;
     }
 }
